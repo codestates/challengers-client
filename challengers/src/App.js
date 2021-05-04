10d0a12865ecc696d2870246170a2deff5f8f8b5
Alex-Choi <choijeaho86@gmail.com>
2021-05-04 17:58:32 +0900

    reduxApply1

--- challengers/src/App.js
+++ challengers/src/App.js
@@ -11,23 +11,30 @@ import ChangeInfo from "./pages/ChangeInfo";
 import MakeNewChallenges from "./pages/MakeNewChallenges";
 import Follower from "./pages/Follower";
 
+// Redux 관련
+import store from './redux/store';
+import { Provider } from 'react-redux';
+// import { ChallengeData } from "../../data/ChallengeData";
 // Home Component Routing !!!
 
 function App() {
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path="/signup" exact component={Signup} />
-        <Route path="/" exact component={Home} />
-        <Route path="/tag" component={ChallengeList} />
-        <Route path="/pin" component={ChallengePinList} />
-        <Route path="/my-challenges" component={MyChallenges} />
-        <Route path="/change-info" component={ChangeInfo} />
-        <Route path="/challenge" component={MakeNewChallenges} />
-        <Route path="/following" component={Follower} />
-      </Switch>
-    </Router>
+    <Provider store = {store}>
+      <Router>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/" exact component={Home} />
+          <Route path="/tag" component={ChallengeList} />
+          <Route path="/pin" component={ChallengePinList} />
+          <Route path="/my-challenges" component={MyChallenges} />
+          <Route path="/change-info" component={ChangeInfo} />
+          <Route path="/challenge" component={MakeNewChallenges} />
+          <Route path="/following" component={Follower} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
--- challengers/src/pages/Home.jsx
+++ challengers/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Navbar from "../components/Navbar/Nav";
@@ -7,10 +7,24 @@ import ChallengeList from "../components/ChallengeList/ChallengeList";
 import ChallengerList from "../components/ChallengerList/ChallengerList";
 import Footer from "../components/Footerbar/Footer";
 
+import { connect } from 'react-redux'; // react, redux연결
+import {getMyChallengeList,
+  deleteMyChallengeList,
+  addMyChallengeList} from '../redux'; // redux의 Action
+
+
+
+
 //Home 컴포넌트는 App 컴포넌트 대용으로 모든 컴포넌트를 넣어주기 위한 컴포넌트
 // isRight로 css값 변화시켜주기 위한 것.
 // toggle을 사용하여 클릭 시 상태값이 변경될 수 있도록
-const Home = () => {
+const Home = (props) => {
+  // 리덕스의 스테이츠를 불러오는 횟수 제한
+  useEffect(() => {
+    props.getMyChallengeList()
+  },[]);
+
+
   const [isRight, setIsRight] = useState(false);
 
   const toggle = () => {
@@ -33,4 +47,30 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = state => {
+  console.log('mapStateToProps(My Challenge List) : ', state);
+  return {
+      myChallengeList : state.myChallengeList
+  }
+}
+
+const mapDispatchToProps = dispatch => { // 버튼에 영향
+  return {
+      getMyChallengeList : () => {
+          dispatch(getMyChallengeList());
+      },
+      deleteMyChallengeList : () => {
+          dispatch(deleteMyChallengeList());
+      },
+      addMyChallengeList : () => {
+          dispatch(addMyChallengeList());
+      }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
+
+// export default Home;
