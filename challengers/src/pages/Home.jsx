f8c6df30885c9bb33dd7f0a26d59ff3208e97625
Alex-Choi <choijeaho86@gmail.com>
2021-05-04 23:03:25 +0900

    done1

--- challengers/src/pages/Home.jsx
+++ challengers/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Cover from "../components/CoverVideo/Cover";
 import ChallengeList from "../components/ChallengeList/ChallengeList";
 import ChallengerList from "../components/ChallengerList/ChallengerList";
 import Footer from "../components/Footerbar/Footer";
+import axios from "axios";
 
 import { connect } from "react-redux"; // react, redux연결
 import {
@@ -17,10 +18,55 @@ import {
 //Home 컴포넌트는 App 컴포넌트 대용으로 모든 컴포넌트를 넣어주기 위한 컴포넌트
 // isRight로 css값 변화시켜주기 위한 것.
 // toggle을 사용하여 클릭 시 상태값이 변경될 수 있도록
-const Home = (props) => {
+const Home = () => {
   // 리덕스의 스테이츠를 불러오는 횟수 제한
+  const [dataTag, setTag] = useState([]);
+  const [dataFollower, setFollower] = useState([]);
+
   useEffect(() => {
-    props.getMyChallengeList();
+    console.log('axios 부르기---------------->');
+    // axios.get('http://localhost:5000/')
+    //   .then(data => console.log(data))
+    //   .catch(err => console.log(err))
+    //   .finally(() => console.log("axios GET finish"));
+    
+    let dataTag_sample = [
+      {
+        id: 0,
+        title : '11',
+        tag_name : '22'
+      },
+      {
+        id: 1,
+        title : '33',
+        tag_name : '22'
+      },
+      {
+        id: 3,
+        title : '44',
+        tag_name : '55'
+      },
+      {
+        id: 4,
+        title : '66',
+        tag_name : '77'
+      },
+      {
+        id: 5,
+        title : '88',
+        tag_name : '99'
+      },
+    ];
+
+    setTag([...dataTag, ...dataTag_sample])
+    let dataFollower_sample = [
+      {userId : 'aaa', Like : 1},
+      {userId : 'bbb', Like : 2},
+      {userId : 'ccc', Like : 3},
+      {userId : 'ddd', Like : 4}
+    ];
+    setFollower([...dataFollower, ...dataFollower_sample])
+
   }, []);
 
   const [isRight, setIsRight] = useState(false);
@@ -36,8 +82,8 @@ const Home = (props) => {
         <Route path="/main">
           <Navbar toggle={toggle} />
           <Cover />
-          <ChallengeList />
-          <ChallengerList />
+          <ChallengeList dataTag={dataTag}/>
+          <ChallengerList dataFollower={dataFollower}/>
           <Footer />
         </Route>
       </Switch>
@@ -52,21 +98,23 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  // 버튼에 영향
-  return {
-    getMyChallengeList: () => {
-      dispatch(getMyChallengeList());
-    },
-    deleteMyChallengeList: () => {
-      dispatch(deleteMyChallengeList());
-    },
-    addMyChallengeList: () => {
-      dispatch(addMyChallengeList());
-    }
-  };
-};
+export default Home;
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+// const mapDispatchToProps = (dispatch) => {
+//   // 버튼에 영향
+//   return {
+//     getMyChallengeList: () => {
+//       dispatch(getMyChallengeList());
+//     },
+//     deleteMyChallengeList: () => {
+//       dispatch(deleteMyChallengeList());
+//     },
+//     addMyChallengeList: () => {
+//       dispatch(addMyChallengeList());
+//     }
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 // export default Home;
